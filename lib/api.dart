68b52eaa40d6b77269cb60f207a3bde24b23e5f2
lopesj <joao.lopes@studentlab.pt>
2022-08-06 19:19:21 +0100

    List songs screen almost done api call DONE order_by DONE aesthetic screen design MISSING

---
+++ lib/api.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+import 'package:canoa_v3_frontend/models/search_song.dart';
+import 'package:http/http.dart' as http;
+import 'package:canoa_v3_frontend/models/song.dart';
+class API {
+  /// local dev environment
+  static final String _url = "http://127.0.0.1:5000/api=";
+  /// dev environment
+  // static final String _url = "http://192.168.1.76:5000/api=";
+
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/search_song.json');
+  }
+
+  static Future<List<SearchSong>> fetchAvailableSongs() async {
+    try {
+      print(_url + "get_available_songs");
+      var response = await http.get(Uri.parse(_url + "get_available_songs"));
+      var body = json.decode(response.body);
+      var res = body["data"].map<SearchSong>(SearchSong.fromJson).toList();
+      return res;
+
+    } catch (e){
+      throw Exception();
+    }
+  }
+}
---
+++ lib/list_songs_screen.dart
@@ -0,0 +1,120 @@
+import 'package:canoa_v3_frontend/models/search_song.dart';
+import 'package:canoa_v3_frontend/models/song.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:canoa_v3_frontend/api.dart';
+
+class ListSongsScreen extends StatefulWidget {
+  const ListSongsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListSongsScreen> createState() => _ListSongsScreenState();
+}
+
+class _ListSongsScreenState extends State<ListSongsScreen> {
+  String dropdownValue = "Most Recent";
+  Future<List<SearchSong>> futureSongs = API.fetchAvailableSongs();
+
+  var items = [
+    "Alphabetically",
+    "Most Recent",
+    "Oldest"
+  ];
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: const Text("HELLO"),
+      centerTitle: true,
+    ),
+    body: Row(
+      children: [
+        Expanded(
+          child: Column(
+            children: [
+                Center(
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: FutureBuilder<List<SearchSong>>(
+                        future: futureSongs,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            List<SearchSong> songs = sortSongs(snapshot.data!, dropdownValue);
+                            return buildSongs(songs);
+                          } else {
+                            return const CircularProgressIndicator();
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+            ],
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              child: Align(
+                alignment: Alignment.topRight,
+                child: DropdownButton(
+                  items: items.map((String items) {
+                    return DropdownMenuItem(
+                      value: items,
+                      child: Text(items),
+                    );
+                  }).toList(),
+                  value: dropdownValue,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownValue = newValue!;
+                    });
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+
+  Widget buildSongs(List<SearchSong> songs) => ListView.builder(
+      itemCount: songs.length,
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        final song = songs[index];
+
+        return Card(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(song.title),
+            ],
+          ),
+          color: Colors.white,
+          margin: EdgeInsets.all(8),
+
+        );
+      },
+  );
+
+  List<SearchSong> sortSongs(List<SearchSong> list, String sortBy) {
+    List<SearchSong> res = list;
+    if (sortBy == "Most Recent") {
+      res.sort((a, b) => a.id.compareTo(b.id));
+    }
+    else if (sortBy == "Oldest") {
+      res.sort((a, b) => (a.id*-1).compareTo(b.id*-1));
+    }
+    else if (sortBy == "Alphabetically") {
+      res.sort((a, b) => a.title.compareTo(b.title));
+    }
+    return res;
+  }
+}
+
